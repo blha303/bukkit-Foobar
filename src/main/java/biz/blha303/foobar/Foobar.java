393c8e1dd27b7e4e02e07b2252b050151d1df955
blha303 <stevensmith.ome@gmail.com>
2014-04-01 02:01:49 +0800

    Initial

---
+++ src/main/java/biz/blha303/foobar/Foobar.java
@@ -0,0 +1,19 @@
+package biz.blha303.foobar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
+import org.bukkit.command.ConsoleCommandSender;
+
+public class Foobar extends JavaPlugin {
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			sender.sendMessage("[Foobar] foo");
+		} else if (sender instanceof ConsoleCommandSender) {
+			sender.sendMessage("[Foobar] bar");
+		} else {
+			sender.sendMessage("[Foobar] foobar");
+		}
+	}
+
+}
